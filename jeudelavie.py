5b3e6e35c702f72b5a78da0a6e2afeeec4493812
Nils Barrellon <nils.rodin@gmail.com>
2021-09-22 10:11:06 +0200

    reorganisation du code
    les fonctions ont été mises en méthodes de classe

--- jeudelavie.py
+++ jeudelavie.py
@@ -22,43 +22,47 @@ class Grille:
     def set_case(self, x : int, y : int, etat : Etat):
         self.contenu[x][y] = etat
         
+        
     def get_case(self, x : int, y : int):
         return self.contenu[x][y]
 
-def voisin(x, y, direction, depart):
-    hauteur, largeur = len(depart), len(depart[0])
-    delta_x, delta_y = direction
-    x_new, y_new = x + delta_x, y + delta_y
-    if x_new < 0 or x_new >= hauteur or y_new < 0 or y_new >= largeur:
-        return Etat.DEAD
-    else:
-        return depart[x_new][y_new]
 
-def nb_voisins(x, y, depart):
-    total_voisins = 0
-    for direction in [(-1,-1), (0,-1), (1,-1), (1,0),(1,1),(0,1),(-1,1), (-1,0)]:
-        if voisin(x, y, direction, depart):
-            total_voisins += 1
-    return total_voisins
-    
-
-def futur_etat(x, y, depart, destination):
-    if depart[x][y]:
-        if 2 <= nb_voisins(x, y, depart) <= 3:
-            destination[x][y] = Etat.ALIVE
+    def voisin(self,x, y, direction):
+        delta_x, delta_y = direction
+        x_new, y_new = x + delta_x, y + delta_y
+        if x_new < 0 or x_new >= self.hauteur or y_new < 0 or y_new >= self.largeur:
+            return Etat.DEAD
         else:
-            destination[x][y] = Etat.DEAD
-    else:
-        destination[x][y] = Etat.ALIVE if nb_voisins(x, y, depart) == 3 else Etat.DEAD
+            return self.contenu[x_new][y_new]
+    
+    
+    def nb_voisins(self,x,y,borne=8)->int:
+        total_voisins = 0
+        for direction in [(-1,-1), (0,-1), (1,-1), (1,0),(1,1),(0,1),(-1,1), (-1,0)]:
+            if self.voisin(x, y, direction):
+                total_voisins += 1
+                if total_voisins==borne:
+                    return total_voisins
+        return total_voisins
+    
     
 class Moteur:
     def __init__(self):
         pass
     
+
+    def futur_etat(self,x, y, grille:Grille, grilleModifiee:Grille):
+        if grille.get_case(x,y)==Etat.ALIVE:
+            if 2 <= grille.nb_voisins(x,y,4) <= 3:
+                grilleModifiee.set_case(x,y,Etat.ALIVE)
+        elif grille.nb_voisins(x,y,4) == 3 :
+            grilleModifiee.set_case(x,y,Etat.ALIVE) 
+    
+    
     def nextgen(self, grille : Grille):
-        tmp = [[elm for elm in ligne] for ligne in grille.contenu]
+        tmp=Grille(4,8)
         for x in range(grille.hauteur):
             for y in range(grille.largeur):
-                futur_etat(x, y, grille.contenu, tmp)
-        grille.contenu = tmp
+                self.futur_etat(x, y, grille, tmp)
+        grille.contenu = tmp.contenu
                 
