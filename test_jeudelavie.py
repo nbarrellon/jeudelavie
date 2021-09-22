afb71faff268090ec8bbf55292a5ff608e51510a
Nils Barrellon <nils.rodin@gmail.com>
2021-09-22 09:37:38 +0200

    Jeu de test augmenté
    ajouté testGetSet sur grille
    ajouté testTailleGrille

--- test_jeudelavie.py
+++ test_jeudelavie.py
@@ -5,9 +5,30 @@ Created on Thu Sep 16 16:55:48 2021
 
 @author: nilsbarrellon
 """
-
 from jeudelavie import Grille,Etat,Moteur
 
+
+def testTailleGrille():
+    grille=Grille(4,8)
+    assert grille.hauteur==4,"La grille n'a pas la hauteur spécifiée lors de la construction"
+    assert grille.largeur==8,'Largueur non valide'
+
+
+def testGetSet():
+    grille=Grille(4,8)
+    grille.set_case(2,2,Etat.ALIVE)
+    assert grille.get_case(2,2)==Etat.ALIVE
+    grille.set_case(2, 2, Etat.DEAD)
+    assert grille.get_case(2, 2)==Etat.DEAD
+    grille=Grille(4,8)
+
+def testEtatParDefaut():
+    grille=Grille(4,8)
+    for i in range(grille.hauteur):
+        for j in range(grille.largeur):
+            assert grille.get_case(i,j)==Etat.DEAD,'Case '+str((i,j))+' dans un mauvais etat'
+    
+ 
 def testMort():
     grille=Grille(4,8)
     grille.set_case(2,2,Etat.ALIVE)
